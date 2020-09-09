3ae601c92316da9caa06095f53f1e09a7560e152
Felicià MAVIANE <felicia.maviane-macia@inrae.fr>
2020-09-09 09:53:14 +0200

    Sem-Ver: bugfix Commit before adding IPAPI

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,10 +3,38 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Length
 from flask_babel import _, lazy_gettext as _l
+from wtforms import FileField
+
 from app.models import User
 
 
-class LaunchForm(FlaskForm):
-    script_or_stored_state = StringField(
-        _l("Script or stored state"), validators=[DataRequired()]
-    )
+class LoadScriptOrStateForm(FlaskForm):
+    script_or_stored_state = FileField(_l("Load a script or a stored state"))
+
+
+class CommonOptions(FlaskForm):
+    pass
+
+
+class ScriptOptions(FlaskForm):
+    pass
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField("Submit")
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField(_l("Username"), validators=[DataRequired()])
+    about_me = TextAreaField(_l("About me"), validators=[Length(min=0, max=140)])
+    submit = SubmitField(_l("Submit"))
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError(_("Please use a different username."))
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,21 +1,18 @@
+import os
 from datetime import datetime
-from flask import (
-    render_template,
-    flash,
-    redirect,
-    url_for,
-    request,
-    g,
-    jsonify,
-    current_app,
-)
+import json
+import logging
+from flask import render_template, flash, redirect, url_for, request, g, jsonify, session
 from flask_login import current_user, login_required
 from flask_babel import _, get_locale
+from werkzeug.utils import secure_filename
 from app import db
 from app.models import User
 from app.translate import translate
 from app.main import bp
-from app.main.forms import LaunchForm
+from app.main.forms import LoadScriptOrStateForm, EmptyForm, EditProfileForm
+
+logger = logging.getLogger(__name__)
 
 
 @bp.before_app_request
@@ -28,12 +25,58 @@ def before_request():
 
 @bp.route("/", methods=["GET", "POST"])
 @bp.route("/index", methods=["GET", "POST"])
-@login_required
 def index():
-    form = LaunchForm()
+    return render_template("index.html")
+
+
+@bp.route("/launch", methods=["GET", "POST"])
+@login_required
+def launch():
+    upload_form = LoadScriptOrStateForm()
+    if upload_form.validate_on_submit():
+        try:
+            j = json.load(upload_form.script_or_stored_state.data)
+            if "script" in j:
+                session["stored_state"] = j
+            elif "title" in j:
+                session["pipeline"] = j
+            else:
+                flash(_("Unknown file"))
+            print(j)
+        except Exception as e:
+            flash(_("Unable to load file, only IPSO Phen files are allowed"))
+            logger.exception(repr(e))
 
     return render_template(
-        "index.html",
+        "launch.html",
         title=_("Launch"),
+        upload_form=upload_form,
+    )
+
+
+@bp.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    form = EmptyForm()
+    return render_template(
+        "user.html",
+        user=user,
         form=form,
     )
+
+
+@bp.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash(_("Your changes have been saved."))
+        return redirect(url_for("main.edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title=_("Edit Profile"), form=form)
